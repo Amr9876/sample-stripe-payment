7178426ce83a4e1ddb9a819582a0228cf2dac53f
Amr9876 <96480723+Amr9876@users.noreply.github.com>
2022-06-20 09:49:48 +0200

    Initial Commit

---
+++ server.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.static('public'));
+
+const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY);
+
+let baseUrl;
+
+if (process.env.NODE_ENV === 'production') {
+	baseUrl = '';
+} else {
+	baseUrl = 'http://localhost:3000';
+}
+
+
+app.post('/create-checkout-session', async (req, res) => {
+
+	const session = await stripe.checkout.sessions.create({
+		line_items: [
+			{
+				price: process.env.STRIPE_PRODUCT_PRICE_ID,
+				quantity: 1
+			}
+		],
+		mode: 'payment',
+		success_url: `${baseUrl}/success.html`,
+		cancel_url: `${baseUrl}/cancel.html`
+	});
+
+	res.redirect(303, session.url);
+
+})
+
+app.listen(3000);
